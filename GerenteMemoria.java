f0ad44ad900f990a6bd3c47837486ba24caf5c49
Joseph Affeldt <joseph.affeldt@dbccompany.com.br>
2020-04-23 16:04:37 -0300

    criado prints e identação.

--- GerenteMemoria.java
+++ GerenteMemoria.java
@@ -1,10 +1,7 @@
 
 public class GerenteMemoria {
-    private String[] s, arquivo;
-    private Ler l;
     private int[] b = new int[8];
     private CPU cpu;
-    private int limiteInf, limiteSup;
 
     public GerenteMemoria() {
         for (int i = 0; i < 8; i++) {
@@ -12,33 +9,33 @@ public class GerenteMemoria {
         }
 
     }
-    public int alocar(int ID){
-        int aux=-1;
-       
-            for(int i = 0; i<8; i++){
-                if(b[i]==ID){
-                aux = i+1;
-                }
+
+    public int alocar(int ID) {
+        int aux = -1;
+
+        for (int i = 0; i < 8; i++) {
+            if (b[i] == ID) {
+                aux = i + 1;
             }
-            if(aux != -1){
-                return aux*128;
-            }
-            else {
-                for(int i=0; i<8; i++){
-                    if(b[i]==-1){
+        }
+        if (aux != -1) {
+            return aux * 128;
+        } else {
+            for (int i = 0; i < 8; i++) {
+                if (b[i] == -1) {
                     b[i] = ID;
-                    return (i+1)*128;
-                    }
+                    return (i + 1) * 128;
                 }
             }
-       return aux;
+        }
+        return aux;
     }
 
     public void Desaloca(int limiteSup) {
-        for (int i = limiteSup-127; i < limiteSup; i++) {
+        for (int i = limiteSup - 127; i < limiteSup; i++) {
             cpu.desalocaPC(i);
         }
-        
+
     }
 
 }
