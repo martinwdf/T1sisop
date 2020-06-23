ac4cce04efc0f8bfccae115dcac8f2c32ac7510b
Martinwdf <martinwdf@gmail.com>
2020-06-23 13:49:31 -0300

    Rot Timer e Memoria

---
+++ controllers/RotTimer.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import java.util.concurrent.Semaphore;
+
+import models.FilaDeProntos;
+import models.PCB;
+
+public class RotTimer {
+    Semaphore semaSch;
+    FilaDeProntos prontos;
+    
+
+    public RotTimer(Semaphore semaSch, FilaDeProntos prontos){
+        this.semaSch = semaSch;
+        this.prontos = prontos;
+    }
+    public void tratamento(PCB pcb){
+        prontos.addPronto(pcb);
+        semaSch.notifyAll();
+    }
+
+}
---
+++ models/Memoria.java
@@ -0,0 +1,23 @@
+package models;
+
+public class Memoria {
+    private Label[] memoria;
+
+    public Memoria(){
+        memoria = new Label[1024];
+    }
+    public Label[] getMemoria(){return memoria;}
+
+    public void setMemoria(Label[] memoria){
+        this.memoria = memoria;
+    }
+
+    public String toString(){
+        String s ="";
+        for(int i=0; i<1024; i++ ){
+            s = memoria[i].print();
+        }
+        return s;
+    }
+
+}
